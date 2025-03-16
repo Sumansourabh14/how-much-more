b92c73eee17db4c3719ab13e3381a1e1d2eeb3ad
Suman Sourabh <sumsourabh14@gmail.com>
2025-03-16 16:26:14 +0530

    created separate form page

---
+++ app/item/upload/page.tsx
@@ -0,0 +1,14 @@
+import UploadItemForm from "@/components/forms/UploadItemForm";
+import React from "react";
+
+const UploadItem = () => {
+  return (
+    <div>
+      <div className="max-w-[500px] mx-auto">
+        <UploadItemForm />
+      </div>
+    </div>
+  );
+};
+
+export default UploadItem;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import UploadItemForm from "@/components/forms/UploadItemForm";
-
 export default function Home() {
   return (
     <div className=" min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
         <h1>How much more?</h1>
-        <div className="max-w-[500px] mx-auto">
-          <UploadItemForm />
-        </div>
       </div>
     </div>
   );
